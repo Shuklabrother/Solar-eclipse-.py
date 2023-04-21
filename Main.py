3ee7177a6743d109b6fb03778ba30ffab4e3eb35
Code with Shukla Brother <90129105+Shuklabrother@users.noreply.github.com>
2023-04-21 13:35:28 +0530

    Create Main.py

---
+++ Main.py
@@ -0,0 +1,19 @@
+from turtle import *
+
+bgcolor('black')
+
+speed(0)
+
+hideturtle()
+
+for i in range(200):
+
+	color('red')	circle(3)
+
+	color('orange')
+
+	circle(3)
+
+	right(3)
+
+	forward(3)
